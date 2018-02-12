3cd65416f7a1822ac9c4529cc39f661765b5e747
Vick Kumar <vick.kumar44@gmail.com>
2018-02-12 15:38:46 -0500

    liked navbar brand

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,28 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Stock App</a>
+    <router-link to="/" class="navbar-brand"><a>Stock App</a></router-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <router-link to="/portfolio" class="nav-link" active-class="active"><a>Portfolio</a></router-link>
-      <router-link to="/stocks" class="nav-link" active-class="active"><a>Stocks</a></router-link>
-    </ul>
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item"><a class="nav-link" href="#">End Day</a></li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Save &amp; Load
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Save Data</a>
-          <a class="dropdown-item" href="#">Load Data</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <router-link to="/portfolio" class="nav-link" active-class="active"><a>Portfolio</a></router-link>
+        <router-link to="/stocks" class="nav-link" active-class="active"><a>Stocks</a></router-link>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a class="nav-link" href="#">End Day</a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Save &amp; Load
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 <style>
   .navbar{
