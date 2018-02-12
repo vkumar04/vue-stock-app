94c45d2f643300b1f5ff8fb1dfb1a9aa06c894e4
Vick Kumar <vick.kumar44@gmail.com>
2018-02-12 15:17:21 -0500

    added router links to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    <h1>Header Component</h1>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Stock App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <router-link to="/portfolio" class="nav-link" active-class="active"><a>Portfolio</a></router-link>
+      <router-link to="/stocks" class="nav-link" active-class="active"><a>Stocks</a></router-link>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Save &amp; Load
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
   </div>
+</nav>
 </template>
+<style>
+  .navbar{
+    margin-top: 60px;
+  }
+</style>
+
