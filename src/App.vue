fc52f9c6ac66363e27215f09501aa15b584dd4b8
Vick Kumar <vick.kumar44@gmail.com>
2018-02-12 15:17:02 -0500

    removed css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="container">
+    <appHeader></appHeader>
     <router-view></router-view>
+    <input type="text" ref="input">
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
-
+  components: {
+    appHeader: Header
+  },
+  data: {
+    
+  }
 }
 </script>
+<style lang="css" scoped>
+
+</style>
