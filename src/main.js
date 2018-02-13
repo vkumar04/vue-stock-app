eb7844faf52eaa5cf812e0717fe2e7a6f6017ded
Vick Kumar <vick.kumar44@gmail.com>
2018-02-13 09:45:22 -0500

    added vuex to vue instance

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes';
-
+import { store } from './store/store';
 Vue.use(VueRouter);
 
 const router = new VueRouter({
@@ -12,5 +12,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
