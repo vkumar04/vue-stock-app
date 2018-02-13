124a3b30bc89154e76094a257d271e59e973bb9a
Vick Kumar <vick.kumar44@gmail.com>
2018-02-13 11:43:20 -0500

    default object returns stocks store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import Vue from  'vue';
 import Vuex from 'vuex';
 
+import stocks from './modules/stocks';
+
 Vue.use(Vuex);
 
-export const store = new Vuex.Store({
-  state: {
-    
+export default new Vuex.Store({
+  modules: {
+    stocks
   }
 })
