3adfd8fa9d4649ccc40e50faefcc435939ebb21b
Vick Kumar <vick.kumar44@gmail.com>
2018-02-13 11:44:34 -0500

    get stocks from store

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -9,17 +9,13 @@
   </div>
 </template>
 <script>
+import {mapGetters} from 'vuex';
 import Stock from './Stock.vue';
 export default {
-  data() {
-    return {
-      stocks: [
-        {id: 1, name: 'BMW', price: 110},
-        {id: 2, name: 'Google', price: 200},
-        {id: 3, name: 'Apple', price: 250},
-        {id: 4, name: 'Twitter', price: 8},
-      ]
-    }
+  computed:{
+    ...mapGetters([
+      'stocks',
+    ])
   },
   components: {
     Stock
