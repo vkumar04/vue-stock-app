0098485b2d33b38b82ee386a71add914c61ec089
Vick Kumar <vick.kumar44@gmail.com>
2018-02-13 11:43:33 -0500

    fixed store import

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes';
-import { store } from './store/store';
+import store from './store/store';
 Vue.use(VueRouter);
 
 const router = new VueRouter({
